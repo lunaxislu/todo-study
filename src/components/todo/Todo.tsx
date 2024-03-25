75150a2bf3cf0e3d7903b99efc4b8120a2ca1b72
lunaxislu <cmk0905@naver.com>
2024-03-25 14:22:15 +0900

    ✅ todo 부모 컴포넌트 완성

--- src/components/todo/Todo.tsx
+++ src/components/todo/Todo.tsx
@@ -1,5 +1,13 @@
-const Todo = () => {
-  return <div></div>;
+import { clsx } from 'clsx';
+interface I_TodoProps extends React.ComponentPropsWithoutRef<'div'> {}
+const Todo = (props: I_TodoProps) => {
+  const { className, children, ...others } = props;
+
+  return (
+    <div className={clsx('max-w-screen-xl my-0 mx-auto min-h-screen md:px-28 xl:px-0', className)} {...others}>
+      {children}
+    </div>
+  );
 };
 
 export default Todo;
