a9031b49704cc1def0ab023e0788d0295dc1fe57
lunaxislu <cmk0905@naver.com>
2024-03-25 14:42:17 +0900

    ✅  todo list logic completed

--- src/components/todo/list/List.tsx
+++ src/components/todo/list/List.tsx
@@ -1,11 +1,24 @@
 import { FunctionComponent, MouseEvent } from 'react';
+import { useSelector } from 'react-redux';
+import { Rootstate } from '../../../store/config/configStore';
+import Todo from '../Todo';
 
 interface I_ListProps {
   onClick: (e: MouseEvent<HTMLDivElement>) => void;
   isDone: boolean;
 }
 const List: FunctionComponent<I_ListProps> = ({ onClick, isDone }) => {
-  return <div className="flex flex-wrap w-full" onClick={onClick}></div>;
+  const todoSlice = useSelector((state: Rootstate) => state.todoSlice);
+
+  return (
+    <div className="flex flex-wrap w-full" onClick={onClick}>
+      {todoSlice
+        .filter((todo) => todo.isDone === isDone)
+        .map((todo) => (
+          <Todo.card key={todo.id} id={todo.id} text={todo.text} title={todo.title} isDone={todo.isDone} />
+        ))}
+    </div>
+  );
 };
 
 export default List;
