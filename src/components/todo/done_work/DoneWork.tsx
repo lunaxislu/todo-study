fa39940391908db36a754d23e475505ef88d3d26
lunaxislu <cmk0905@naver.com>
2024-03-25 16:46:18 +0900

    ✅  hook injection

--- src/components/todo/done_work/DoneWork.tsx
+++ src/components/todo/done_work/DoneWork.tsx
@@ -1,12 +1,14 @@
+import useChangeTodo from '../../../hooks/useChangeTodo';
 import Todo from '../Todo';
 
 const DoneWork = () => {
+  const { changeTodoStateHandler } = useChangeTodo();
   return (
     <div>
       <Todo.title className="text-2xl" level={2}>
         Done Work 🕶️
       </Todo.title>
-      <Todo.list isDone={true} onClick={} />
+      <Todo.list isDone={true} onClick={changeTodoStateHandler} />
     </div>
   );
 };
--- src/components/todo/to_work/TodoWork.tsx
+++ src/components/todo/to_work/TodoWork.tsx
@@ -1,12 +1,14 @@
+import useChangeTodo from '../../../hooks/useChangeTodo';
 import Todo from '../Todo';
 
 const ToWork = () => {
+  const { changeTodoStateHandler } = useChangeTodo();
   return (
     <div className="mt-14">
       <Todo.title className="text-2xl" level={2}>
         To Work 📣
       </Todo.title>
-      <Todo.list isDone={false} onClick={} />
+      <Todo.list isDone={false} onClick={changeTodoStateHandler} />
     </div>
   );
 };
