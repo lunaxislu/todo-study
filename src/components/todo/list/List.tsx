d207b17558c3d35e4ffabb684060145b6d276991
lunaxislu <cmk0905@naver.com>
2024-03-25 14:33:08 +0900

    ✅  todo list constructure logic complete

--- src/components/todo/list/List.tsx
+++ src/components/todo/list/List.tsx
@@ -1,5 +1,11 @@
-const List = () => {
-  return <div></div>;
+import { FunctionComponent, MouseEvent } from 'react';
+
+interface I_ListProps {
+  onClick: (e: MouseEvent<HTMLDivElement>) => void;
+  isDone: boolean;
+}
+const List: FunctionComponent<I_ListProps> = ({ onClick, isDone }) => {
+  return <div className="flex flex-wrap w-full" onClick={onClick}></div>;
 };
 
 export default List;
