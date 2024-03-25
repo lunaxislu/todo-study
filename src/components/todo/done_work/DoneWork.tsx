abbd8d3052bff9c6e29c51290519f76a7d3948ab
lunaxislu <cmk0905@naver.com>
2024-03-25 16:45:12 +0900

    🔥  불필요 로직 remove

--- src/components/todo/done_work/DoneWork.tsx
+++ src/components/todo/done_work/DoneWork.tsx
@@ -1,32 +1,12 @@
-import { MouseEvent } from 'react';
-import { useDispatch } from 'react-redux';
-import { AppDispatch } from '../../../store/config/configStore';
-import { switchTodo } from '../../../store/module/todoSlice';
 import Todo from '../Todo';
 
 const DoneWork = () => {
-  const dispatch = useDispatch<AppDispatch>();
-  const changeTodoHandler = (e: MouseEvent<HTMLElement>) => {
-    if (e.target instanceof HTMLButtonElement) {
-      console.log(e.target.id);
-      console.log(e.target.name);
-      if (e.target.name === 'switch') {
-        console.log;
-        switchTodoStateHandler(e.target.id);
-      }
-    }
-  };
-
-  const switchTodoStateHandler = (id: string) => {
-    console.log(id);
-    dispatch(switchTodo(id));
-  };
   return (
     <div>
       <Todo.title className="text-2xl" level={2}>
         Done Work 🕶️
       </Todo.title>
-      <Todo.list isDone={true} onClick={changeTodoHandler} />
+      <Todo.list isDone={true} onClick={} />
     </div>
   );
 };
--- src/components/todo/to_work/TodoWork.tsx
+++ src/components/todo/to_work/TodoWork.tsx
@@ -1,37 +1,12 @@
-import { MouseEvent } from 'react';
-import { useDispatch } from 'react-redux';
-import { AppDispatch } from '../../../store/config/configStore';
-import { removeTodo, switchTodo } from '../../../store/module/todoSlice';
 import Todo from '../Todo';
 
 const ToWork = () => {
-  const dispatch = useDispatch<AppDispatch>();
-  const changeTodoHandler = (e: MouseEvent<HTMLElement>) => {
-    if (e.target instanceof HTMLButtonElement) {
-      if (e.target.name === 'switch') {
-        console.log;
-        switchTodoStateHandler(e.target.id);
-      }
-
-      if (e.target.name === 'remove') {
-        removeTodoHandler(e.target.id);
-      }
-    }
-  };
-
-  const switchTodoStateHandler = (id: string) => {
-    console.log(id);
-    dispatch(switchTodo(id));
-  };
-  const removeTodoHandler = (id: string) => {
-    dispatch(removeTodo(id));
-  };
   return (
     <div className="mt-14">
       <Todo.title className="text-2xl" level={2}>
         To Work 📣
       </Todo.title>
-      <Todo.list isDone={false} onClick={changeTodoHandler} />
+      <Todo.list isDone={false} onClick={} />
     </div>
   );
 };
