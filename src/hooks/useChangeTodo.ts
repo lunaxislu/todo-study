541712ffb6034922c27419a1195a30aa2102cabd
lunaxislu <cmk0905@naver.com>
2024-03-25 16:44:39 +0900

    ✅  hook logic completed

--- src/hooks/useChangeTodo.ts
+++ src/hooks/useChangeTodo.ts
@@ -1,1 +1,31 @@
+import { MouseEvent } from 'react';
+import { useDispatch } from 'react-redux';
+import { AppDispatch } from '../store/config/configStore';
+import { removeTodo, switchTodo } from '../store/module/todoSlice';
 
+const useChangeTodo = () => {
+  const dispatch = useDispatch<AppDispatch>();
+  const changeTodoStateHandler = (e: MouseEvent<HTMLElement>) => {
+    if (e.target instanceof HTMLButtonElement) {
+      switch (e.target.name) {
+        case 'switch':
+          switchTodoState(e.target.id);
+          break;
+        case 'remove':
+          removeTodoSate(e.target.id);
+      }
+    }
+  };
+
+  const switchTodoState = (id: string) => {
+    dispatch(switchTodo(id));
+  };
+
+  const removeTodoSate = (id: string) => {
+    dispatch(removeTodo(id));
+  };
+
+  return { changeTodoStateHandler };
+};
+
+export default useChangeTodo;
