da2168e31cbfe557291b70e6f7127123fb6a0ca3
lunaxislu <cmk0905@naver.com>
2024-03-25 14:23:53 +0900

    ✅  button 컴포넌트 완성

--- src/components/todo/button/Button.tsx
+++ src/components/todo/button/Button.tsx
@@ -1,5 +1,17 @@
-const Button = () => {
-  return <div></div>;
+import clsx from 'clsx';
+import { FunctionComponent } from 'react';
+
+interface I_ButtonProps extends React.ComponentPropsWithoutRef<'button'> {
+  type: 'submit' | 'reset' | 'button';
+}
+
+const Button: FunctionComponent<I_ButtonProps> = (props) => {
+  const { type, className, children = '버튼', ...others } = props;
+  return (
+    <button type={type} className={clsx('border w-32 h-10 p-2', className)} {...others}>
+      {children}
+    </button>
+  );
 };
 
 export default Button;
